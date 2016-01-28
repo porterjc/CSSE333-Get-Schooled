6026394b2188144a6683f0eec9e6d6b9c6f84780
Jack Porter <porterjc@rose-hulman.edu>
2016-01-27 22:02:10 -0500

    Added basic functionality for connecting to the DB and switching layouts

--- GetSchooled/app/src/main/java/com/example/porterjc/getschooled/MainActivity.java
+++ GetSchooled/app/src/main/java/com/example/porterjc/getschooled/MainActivity.java
@@ -1,25 +1,89 @@
 package com.example.porterjc.getschooled;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.sql.SQLClientInfoException;
+import java.sql.SQLDataException;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class MainActivity extends AppCompatActivity {
-    EditText username = (EditText)findViewById(R.id.username);
-    EditText password = (EditText)findViewById(R.id.password);
+    ServerConnectClass connection;
+    EditText username;
+    EditText password;
+    Button loginB;
+    Button createAccB;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        connection = new ServerConnectClass();
+        username= (EditText)findViewById(R.id.username);
+        password = (EditText)findViewById(R.id.password);
+        loginB = (Button)findViewById(R.id.login);
+        createAccB = (Button)findViewById(R.id.newAccount);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        createAccB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login(v);
+                Connection con = connection.connect();
+                if (con != null) {
+                    try {
+                        username.setText(con.getCatalog());
+                        String query = "";
+                        //Statement stmt = con.createStatement();
+                        //ResultSet rs = stmt.executeQuery(query);
+                        //login(v);
+                    }
+                    catch (SQLException mSQLException) {
+                        if(mSQLException instanceof SQLClientInfoException){
+                            //some toast message to user.
+                        }else if(mSQLException instanceof SQLDataException) {
+                            //some toast message to user.
+                        }else{
+
+                        }
+                    }
+                }
+            }
+        });
+
+        loginB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login(v);
+                Connection con = connection.connect();
+                if (con != null) {
+                    username.setText("It worked!");
+                    String query = "";
+                    //Statement stmt = con.createStatement();
+                    //ResultSet rs = stmt.executeQuery(query);
+                    login(v);
+                }
+            }
+        });
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -54,10 +118,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void login(View view) {
-        if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
-            //correcct password
-        } else {
-            //wrong password
-        }
+        DialogFragment dialogFragment = new DialogFragment() {
+            @Override
+            public Dialog onCreateDialog(Bundle savedInstanceState) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(
+                        getActivity());
+                // Inflate View
+                LayoutInflater inflater = getActivity().getLayoutInflater();
+                View view = inflater.inflate(R.layout.top_student, null);
+                builder.setView(view);
+                return builder.create();
+            }
+        };
+        dialogFragment.show(getFragmentManager(), null);
     }
 }
--- GetSchooled/app/src/main/java/com/example/porterjc/getschooled/ServerConnectClass.java
+++ GetSchooled/app/src/main/java/com/example/porterjc/getschooled/ServerConnectClass.java
@@ -11,11 +11,11 @@ import java.sql.SQLException;
  * Created by porterjc on 1/26/2016.
  */
 public class ServerConnectClass {
-    String ip;
+    String ip = "titan.csse.rose-hulman.edu";
     String clazz = "net.sourceforge.jtds.jdbc.Driver";
-    String dataB;
-    String usernDB;
-    String passwDB;
+    String dataB = "GetSchooledDatabase";
+    String usern;
+    String passw;
 
     public Connection connect() {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
@@ -26,8 +26,7 @@ public class ServerConnectClass {
         try {
             Class.forName(clazz);
             URL = "jdbc:jtds:sqlserver://" + ip + ";"
-                    + "databaseName=" + dataB + ";user=" + usernDB + ";password="
-                    + passwDB + ";";
+                    + "databaseName=" + dataB + ";user=porterjc;password=password;";
             connection = DriverManager.getConnection(URL);
         } catch (SQLException se) {
             Log.e("ERRO", se.getMessage());
@@ -37,5 +36,9 @@ public class ServerConnectClass {
             Log.e("ERRO", e.getMessage());
         }
         return connection;
+    }
+
+    public String getIP() {
+        return ip;
     }
 }
