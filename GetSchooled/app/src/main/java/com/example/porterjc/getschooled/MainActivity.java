e5cc61fb5e85b3735f6d1a00e8df7882787ba84b
Jack Porter <porterjc@rose-hulman.edu>
2016-02-03 20:05:52 -0500

    Added updated JDBS jar and new query attempts.

--- GetSchooled/app/src/main/java/com/example/porterjc/getschooled/MainActivity.java
+++ GetSchooled/app/src/main/java/com/example/porterjc/getschooled/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.porterjc.getschooled;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.sql.PreparedStatement;
 import java.sql.SQLClientInfoException;
 import java.sql.SQLDataException;
 import java.sql.SQLException;
