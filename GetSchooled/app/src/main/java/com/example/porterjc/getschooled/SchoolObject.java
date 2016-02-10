ef0cca0b8e1ba8383c2ae32c2b54ecb3de944200
schmidt-kassandra <schmidkl@rose-hulman.edu>
2016-02-09 22:50:18 -0500

    - Finished User Profile XML - Created ListView Item - Started School Object Class - Started ArrayAdapter for ListView - Added new drawable objects

---
+++ GetSchooled/app/src/main/java/com/example/porterjc/getschooled/SchoolObject.java
@@ -0,0 +1,22 @@
+package com.example.porterjc.getschooled;
+
+/**
+ * Created by schmidkl on 2/9/2016.
+ */
+public class SchoolObject {
+
+    public String mSchoolName;
+    public String mSchoolPictureAddress;
+
+    public SchoolObject() {
+        super();
+    }
+
+    public SchoolObject(String schoolName, String pictureAddress) {
+        mSchoolName = schoolName;
+        mSchoolPictureAddress = pictureAddress;
+    }
+
+    // Add Getters & Setters
+
+}
